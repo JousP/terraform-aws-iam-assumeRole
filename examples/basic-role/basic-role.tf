d109526d99e11b3cdd0e56cf6e1af4e8318cf885
Julie "Jous" Paillusseau <julie.paillusseau@nbs-system.com>
2019-11-01 09:41:41 +0100

    version 1.0.0 compatible with terraform 0.11

--- examples/basic-role/basic-role.tf
+++ examples/basic-role/basic-role.tf
@@ -1,5 +1,5 @@
 module "role_basic" {
-  source         = "github.com/JousP/terraform-aws-iam-assumeRole"
+  source         = "github.com/JousP/terraform-aws-iam-assumeRole?ref=v1.0.0"
   name           = "basic"
   description    = "Basic role that every IAM users in this account can assume to have ReadOnly access"
   policies_count = 1
--- examples/service-role/service-role.tf
+++ examples/service-role/service-role.tf
@@ -1,5 +1,5 @@
 module "ec2_role" {
-  source         = "github.com/JousP/terraform-aws-iam-assumeRole"
+  source         = "github.com/JousP/terraform-aws-iam-assumeRole?ref=v1.0.0"
   name           = "ec2_role"
   description    = "This role can be assumed by the EC2 service to get ReadOnly access to the AWS account."
   service        = "ec2.amazonaws.com"
